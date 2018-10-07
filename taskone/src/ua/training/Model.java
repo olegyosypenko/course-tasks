ce04b58f7e8c8600e824fff57208784ed8d079f5
Oleg Yosypenko <yosypenko.oleg@gmail.com>
2018-10-07 14:37:21 +0300

    refactor model

--- taskone/src/ua/training/Model.java
+++ taskone/src/ua/training/Model.java
@@ -3,11 +3,16 @@ package ua.training;
 public class Model {
 
     public static final String SPACE = " ";
+    public static final String EXCLAMATION_MARK = "!";
+    public static final String HELLO = "Hello";
+    public static final String WORLD = "world";
 
     private StringBuilder message;
+    private String[] requirements;
 
     public Model() {
         this.message = new StringBuilder();
+        this.requirements = {HELLO, WORLD};
     }
 
 
@@ -16,11 +21,14 @@ public class Model {
     }
 
     public void addLastWordToOurMessage(String word) {
-        this.message.append(word);
+        this.message.append(word).append(EXCLAMATION_MARK);
     }
 
     public String getMessage() {
         return this.message.toString();
     }
 
+    public String[] getRequirements() {
+        return this.requirements;
+    }
 }
