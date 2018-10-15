85738bebbda3531a78d3aecbab2ee4b968d58df7
Oleg Yosypenko <yosypenko.oleg@gmail.com>
2018-10-15 15:36:54 +0300

    add requests

--- taskthree/src/ua/training/controller/Constants.java
+++ taskthree/src/ua/training/controller/Constants.java
@@ -1,4 +1,11 @@
 package ua.training.controller;
 
 public interface Constants {
+    public final String FIRST_NAME_REQUEST = "first name";
+    public final String LAST_NAME_REQUEST = "last name";
+    public final String MOBILE_PHONE_REQUEST = "phone";
+    public final String GROUP_REQUEST = "group";
+    public final String NICKNAME_REQUEST = "nickname";
+    public final String EMAIL_REQUEST = "email";
+    public final String LAST_CHANGE_DATE_REQUEST = " last change date";
 }
--- taskthree/src/ua/training/controller/Controller.java
+++ taskthree/src/ua/training/controller/Controller.java
@@ -1,4 +1,26 @@
 package ua.training.controller;
 
-public class Controller implements RegularExpressions {
+public class Controller implements RegularExpressions, Constants {
+
+
+    public void createEntity() {
+        String firstName = getInputThatMatches(FIRST_NAME_REGULAR, FIRST_NAME_REQUEST);
+        String lastName;
+        String phoneNumber;
+
+
+    }
+
+    public String getInputThatMatches(String regularExpression, String messageToAsk) {
+        String input = EMPTY_STRING;
+        while (!input.matches(regularExpression)) {
+            this.view.askForInput(messageToAsk);
+            input = this.scanner.next();
+            if (!input.matches(regularExpression)) {
+                this.view.printErrorMessage();
+            }
+        }
+        return input;
+    }
+
 }
