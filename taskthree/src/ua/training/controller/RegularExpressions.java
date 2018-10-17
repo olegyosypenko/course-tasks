b5661eb06d61459487d44a0775e96827e5f581eb
oleg <yosypenko.oleg@gmail.com>
2018-10-17 23:29:25 +0300

    add regular expressions

--- taskthree/src/ua/training/controller/RegularExpressions.java
+++ taskthree/src/ua/training/controller/RegularExpressions.java
@@ -9,5 +9,10 @@ public interface RegularExpressions {
     String LAST_CHANGE_DATE_REGULAR_EXPRESSION = "\\d{1,2}[.]{1}\\d{1,2}[.]{1}\\d{4}";
     String LAST_ADD_DATE_REGULAR_EXPRESSION = "\\d{1,2}[.]{1}\\d{1,2}[.]{1}\\d{4}";
     String SKYPE_REGULAR_EXPRESSION = "[a-zA-Z][a-zA-Z0-9\\.,\\-_]{5,31}";
-
+    String ZIPCODE_REGULAR_EXPRESSION = "\\d{5}([ \\-]\\d{4})?";
+    String STREET_REGULAR_EXPRESSION = "^((.){1,}(\\d){1,}(.){0,})$";
+    String CITY_REGULAR_EXPRESSION = "^[a-zA-Z]+(?:[\\s-][a-zA-Z]+)*$";
+    String BUILDING_NUMBER_REGULAR_EXPRESSION = "[0-9]{1,4}";
+    String FLAT_NUMBER_REGULAR_EXPRESSION = "[0-9]{1,4}";
+    String COMMENT_REGULAR_EXPRESSION = "[A-Za-z0-9]";
 }
