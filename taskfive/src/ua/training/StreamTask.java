577e7bb4879c84c3bb109fc7dd89b4a1a588007c
oleg <yosypenko.oleg@gmail.com>
2018-10-24 21:22:38 +0300

    add task solution

--- taskfive/src/ua/training/StreamTask.java
+++ taskfive/src/ua/training/StreamTask.java
@@ -7,15 +7,17 @@ public class StreamTask {
     public static void main(String[] strs) {
         List<Integer> list = Arrays.asList(1, -2, 0, 8, -6, 2, 6);
         double d = list.stream().mapToInt(b->b).average().getAsDouble();
-        int i = list.stream().mapToInt(b->b).sum();
-        list.stream().mapToInt(i1 -> i1);
-        long amountGreaterThanZero = list.stream().filter( i1 -> i1 > 0 ).count();
         long amountEqualsZero = list.stream().filter( i1 -> i1 == 0 ).count();
+        long amountGreaterThanZero = list.stream().filter( i1 -> i1 > 0 ).count();
         System.out.println(list);
         System.out.println(d);
-        System.out.println(i);
-        System.out.println(amountGreaterThanZero);
-        System.out.println(amountEqualsZero);
-
+        list.stream().forEach(i -> {
+            if (i.equals(list.stream().min((i1, i2) -> i1 - i2).get())) {
+                System.out.println("Value i = " + i + ", index = " + list.indexOf(i));
+            }
+        });
+        System.out.println("Number of zero: " + amountEqualsZero);
+        System.out.println("Number of greater than zero: " + amountGreaterThanZero);
+        System.out.println(Arrays.toString(list.stream().map(i1 -> i1 * 5).toArray()));
     }
 }
