e4ee5ec1895aec131fed4e7ef854b00c078c14fa
oleg <yosypenko.oleg@gmail.com>
2018-10-25 11:50:10 +0300

    change second solution

--- taskfive/src/ua/training/StreamTask.java
+++ taskfive/src/ua/training/StreamTask.java
@@ -11,11 +11,8 @@ public class StreamTask {
         long amountGreaterThanZero = list.stream().filter( i1 -> i1 > 0 ).count();
         System.out.println(list);
         System.out.println(d);
-        list.stream().forEach(i -> {
-            if (i.equals(list.stream().min((i1, i2) -> i1 - i2).get())) {
-                System.out.println("Value i = " + i + ", index = " + list.indexOf(i));
-            }
-        });
+        int min = list.stream().min((i1, i2) -> i1 - i2).get();
+        System.out.println("Value i = " + min + ", index = " + list.indexOf(min));
         System.out.println("Number of zero: " + amountEqualsZero);
         System.out.println("Number of greater than zero: " + amountGreaterThanZero);
         System.out.println(Arrays.toString(list.stream().map(i1 -> i1 * 5).toArray()));
