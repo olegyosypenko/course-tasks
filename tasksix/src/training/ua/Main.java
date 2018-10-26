8bf9c76a40cd62041848285c030fc8114a644440
oleg <yosypenko.oleg@gmail.com>
2018-10-27 00:10:51 +0300

    Add stream solution

--- tasksix/src/training/ua/Main.java
+++ tasksix/src/training/ua/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Integer[] arr = {1, 7, -5, 4, 1, 7, 7, 1, -5};
         System.out.println(getNumberOfSame(arr));
+        System.out.println(getNumberOfSameStream(arr));
     }
     public static Map<Integer, Integer> getNumberOfSame(Integer[] arr) {
         Map<Integer, Integer> result = new HashMap<>();
@@ -18,6 +19,19 @@ public class Main {
                 result.put(i, 1);
             }
         }
+        return result;
+    }
+    public static Map<Integer, Integer> getNumberOfSameStream(Integer[] arr) {
+        Map<Integer, Integer> result = new HashMap<>();
+        Stream.of(arr).forEach(i -> {
+            if (result.containsKey(i)) {
+                result.put(i, result.get(i) + 1);
+            }
+            else {
+                result.put(i, 1);
+            }
+
+        });
         return result;
     }
 }
