5b555b80537d8169310a414bb40b0571859b0dbf
oleg <yosypenko.oleg@gmail.com>
2018-10-26 23:11:15 +0300

    add solution

--- tasksix/src/training/ua/Main.java
+++ tasksix/src/training/ua/Main.java
@@ -1,8 +1,23 @@
 package training.ua;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-
+        Integer[] arr = {1, 7, -5, 4, 1, 7, 7, 1, -5};
+        System.out.println(getNumberOfSame(arr));
     }
-
+    public static Map<Integer, Integer> getNumberOfSame(Integer[] arr) {
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int i : arr) {
+            if (result.containsKey(i)) {
+                result.put(i, result.get(i) + 1);
+            }
+            else {
+                result.put(i, 1);
+            }
+        }
+        return result;
+    }
 }
